bcc3cb0e0d741de2039095d1731170be2d9f51fb
eiballesterosm <eduballesteros@gmail.com>
2022-11-07 16:55:30 -0500

    Add Employee service

--- src/app/services/employee.service.ts
+++ src/app/services/employee.service.ts
@@ -1,9 +1,43 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Employee } from '../models/employee-model';
+import { Observable } from 'rxjs';
+import { Subject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EmployeeService {
 
-  constructor() { }
+  readonly APIUrl = "https://localhost:44394/api/employee/";
+
+  formData: Employee;
+
+  constructor(private http: HttpClient) { }
+
+  getEmployeeList(): Observable<Employee[]> {
+    return this.http.get<Employee[]>(this.APIUrl)
+  }
+
+  addEmployee(employee: Employee) {
+    return this.http.post(this.APIUrl, employee);
+  }
+
+  deleteEmployee(employeeID: number) {
+    return this.http.delete(this.APIUrl + employeeID);
+  }
+
+  updateEmployee(employee: Employee) {
+    return this.http.put(this.APIUrl, employee);
+  }
+
+  private _listeners = new Subject<any>();
+
+  listen(): Observable<any> {
+    return this._listeners.asObservable();
+  }
+
+  filter(filterBy: string) {
+    this._listeners.next(filterBy);
+  }
 }
